0d6d8e4bbc1649e2482e47e583b85c8bf2de1258
Aditi <yourgithubemail@gmail.com>
2026-05-23 00:55:23 +0530

    fixed charts ui

--- src/pages/1_analytics.py
+++ src/pages/1_analytics.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.title("📊 Analytics Page")
 
@@ -13,4 +14,17 @@ st.subheader("Top 5 Videos")
 
 top = df.sort_values(by="Views", ascending=False)
 
+df["Short_Title"] = df["Title"].str[:20]
+
+fig = px.bar(
+    df,
+    x="Short_Title",
+    y="Views",
+    color="Channel",
+    title="YouTube Video Views"
+)
+
+fig.update_xaxes(tickangle=0)
+
+st.plotly_chart(fig)
 st.dataframe(top.head())
